6bd3e2b8d1fde0eec1a9b30e8299e2f44a15506e
DylanPerkins <dp011400@gmail.com>
2023-03-11 18:42:55 -0500

    Started Zenva academy, got a bunch of stuff ($1k value) from a humble bundle for $20!

---
+++ Zenva_Academy_Learning/Course_One/AlphaTurtle.py
@@ -0,0 +1,15 @@
+# Imports everything from the library, turtle
+from turtle import *
+
+# Moves forward x pixels
+forward(50)
+# Move to the right, x degrees
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+
+# Keeps window open after above code has finished running
+done() 
