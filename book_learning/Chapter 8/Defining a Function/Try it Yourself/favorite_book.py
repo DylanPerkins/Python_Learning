0a676db8db39044b3c135ef3b93d85fc1e6fb52c
DylanPerkins <dp011400@gmail.com>
2023-03-15 23:08:45 -0400

    Finished Section One of Chapter 8

---
+++ book_learning/Chapter 8/Defining a Function/Try it Yourself/favorite_book.py
@@ -0,0 +1,10 @@
+# Try it Yourself | 8-2 
+# Write a function called favorite_book() that accepts one parameter, title.
+# The function should print a message, such as 'One of my favorite books is Alice in Wonderland'.
+# Call the function, making sure to include a book title as an argument in the function call.
+
+def favorite_book(title):
+    """Display a message about my favorite book."""
+    print(f"One of my favorite books is {title.title()}.")
+
+favorite_book('Scythe')
---
+++ book_learning/Chapter 8/Defining a Function/Try it Yourself/message.py
@@ -0,0 +1,10 @@
+# Try it Yourself | 8-1 
+# Write a function called display_message() that prints one sentence 
+# telling everyone what you are learning about in this chapter. 
+# Call the function, and make sure the message displays correctly.
+
+def display_message():
+    """Display a message about what I'm learning in this chapter."""
+    print("I'm learning about functions in this chapter.")
+
+display_message()
