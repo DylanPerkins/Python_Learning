4e664b868d243822ebe357f5624397dc4bf0678d
DylanPerkins <dp011400@gmail.com>
2023-03-28 22:52:29 -0400

    Pushing changes

--- Zenva_Academy_Learning/Course_Four/EscapeRoom.py
+++ Zenva_Academy_Learning/Course_Four/EscapeRoom.py
@@ -36,3 +36,24 @@ class GameObject:
         return f"You smell the {self.name} and smell {self.smell}.\n"
     
 game_object = GameObject("rock", "a rock", "rough", "dusty")
+
+class Room:
+    # Fields
+    escape_code = 0
+    game_objects = []
+
+    # Initializer
+    def __init__(self, escape_code, game_objects):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
+
+    # Returns whether the entered code equals the escape code
+    def check_code(self, code):
+        return code == self.escape_code
+    
+    # Returns a list of the names of the game objects in the room
+    def get_game_object_names(self):
+        names = []
+        for game_object in self.game_objects:
+            names.append(game_object.name)
+        return names
